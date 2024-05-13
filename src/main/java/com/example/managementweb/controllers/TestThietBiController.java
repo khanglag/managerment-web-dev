d8b2b0bd56919dd5b4b06fbd60bf2a7674f749b9
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-05-13 19:06:42 +0700

    uo

--- src/main/java/com/example/managementweb/controllers/TestThietBiController.java
+++ src/main/java/com/example/managementweb/controllers/TestThietBiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.managementweb.models.dtos.ThietBi.ThietbiReponsDto;
@@ -11,6 +12,7 @@ import com.example.managementweb.models.entities.ThietbiEntity;
 import com.example.managementweb.services.ThietBiService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TestThietBiController {
@@ -22,6 +24,13 @@ public class TestThietBiController {
         List<ThietbiReponsDto> list = thietBiService.findAll();
         model.addAttribute("thietbisList", list);
         return "View/testthietbis";
+    }
+
+    @GetMapping("/thietbis/{id}")
+    public String getThietBiDetail(@PathVariable("id") String id, Model model) {
+        Optional<ThietbiReponsDto> thietbi = thietBiService.findByID(id);
+        model.addAttribute("thietbi", thietbi.get());
+        return "View/thietbiDetail"; // Trả về view chi tiết của thiết bị
     }
 
 }
--- src/main/java/com/example/managementweb/services/interfaces/IThietBiService.java
+++ src/main/java/com/example/managementweb/services/interfaces/IThietBiService.java
@@ -1,15 +1,17 @@
 package com.example.managementweb.services.interfaces;
 
-
 import com.example.managementweb.models.dtos.Thanhvien.ThanhvienReponsDto;
 import com.example.managementweb.models.dtos.ThietBi.ThietbiReponsDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IThietBiService {
 
     List<ThietbiReponsDto> findAll();
+
     List<ThietbiReponsDto> findByTenTB(String tenTB);
 
-   
+    Optional<ThietbiReponsDto> findByID(String id);
+
 }
